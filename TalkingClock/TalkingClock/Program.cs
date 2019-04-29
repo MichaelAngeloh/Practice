80b860ce81a889488b2a727bddd86c8051c56c31
Michael Denis <mdenis@updikedl.com>
2019-04-29 13:10:54 -0700

    created a new project called talking clock

---
+++ TalkingClock/TalkingClock/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkingClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            string time = now.ToString("HH:mm");
+
+            
+
+            Console.Read();
+        }
+    }
+}

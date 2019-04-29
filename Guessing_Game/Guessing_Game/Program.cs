fedfe6d944e1da193ae4db518be2918e37bf14aa
Michael Denis <mdenis@updikedl.com>
2019-04-29 11:12:23 -0700

    prototype

--- Guessing_Game/Guessing_Game/Program.cs
+++ Guessing_Game/Guessing_Game/Program.cs
@@ -10,9 +10,29 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+
+            int random = rnd.Next(1, 10);
+            Console.WriteLine(random);//display random number for test purposes
+
+            //ask user to guess a number between 1 - 10:
+            Console.Write("Guess a number between 1 and 10: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if (random == userInput)
+            {
+                Console.WriteLine("\nCongrats");
+            }
+            else if (random > userInput)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (random < userInput)
+            {
+                Console.WriteLine("Lower");
+            }
 
 
-
+            Console.Read();
         }
     }
 }

49716142f21d61efaec107c4c219977728479702
Michael Denis <mdenis@updikedl.com>
2019-04-30 07:52:10 -0700

    added some code

--- TalkingClock/TalkingClock/Program.cs
+++ TalkingClock/TalkingClock/Program.cs
@@ -10,10 +10,30 @@ namespace TalkingClock
     {
         static void Main(string[] args)
         {
-            DateTime now = DateTime.Now;
-            string time = now.ToString("HH:mm");
+            //DateTime now = DateTime.Now;
+            //string time = now.ToString("HH:mm");
+            string[] teens = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] digits = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] ten = { "ten","twenty","thirty","fourty","fifty" };
 
-            
+            string time = "10:33";
+            string[] timeSplit = time.Split(':');
+            int hour = Convert.ToInt32(timeSplit[0]);
+            int min = Convert.ToInt32(timeSplit[1]);
+
+            //Console.Write("{0},{1}", hour, min);
+
+            Console.Write("The time is ");
+
+            if (hour > 13)
+            {
+                Console.Write("{0}", hour % 12);
+            }
+            else if(hour < 13)
+            {
+                Console.Write("{0} {1} {2}",digits[hour-1],ten[min /10 - 1], digits[min % 10 - 1]);
+            }
+
 
             Console.Read();
         }
